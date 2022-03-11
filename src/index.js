18fe7fd0011014ae3007a8b3a0135f30f0ded86c
soonee <mysiri29@gmail.com>
2022-03-12 08:32:02 +0900

    feature/1.1c.0 squash merge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import hello from './hello.js'
 import world from './world.js'
+import css from '../assets/style.css'
 document.querySelector('#root').innerHTML = hello + ' ' + world
+console.log('css', css)
